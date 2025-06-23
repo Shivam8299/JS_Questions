a371f542c15fbab28949f17634dabbf8a9eab745
Shivam8299 <shivammishra0381@gmail.com>
2025-06-23 23:19:16 +0530

    revision of promises

---
+++ promise.js
@@ -0,0 +1,21 @@
+let car = {
+    brand: "honda city",
+    price: "16 lacks",
+    isavailble: true
+}
+
+let promise = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        const success = true;
+        if(success){
+            resolve(car)
+        }
+        else {
+            reject("rejected due to error")
+        }
+    },1000 )
+})
+
+promise
+.then(data=>console.log("these are your car details",data))
+.catch(error=>console.log(error))
