c3835f3e3f734680d7bdf0ae7c17bb59788aa6f3
Shivam8299 <shivammishra0381@gmail.com>
2025-06-28 15:12:17 +0530

    updated code

--- promise.js
+++ promise.js
@@ -1,21 +1,20 @@
 let car = {
-    brand: "honda city",
-    price: "16 lacks",
-    isavailble: true
-}
+  brand: "honda city",
+  price: "16 lacks",
+  isavailble: true,
+};
 
-let promise = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        const success = true;
-        if(success){
-            resolve(car)
-        }
-        else {
-            reject("rejected due to error")
-        }
-    },1000 )
-})
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const success = true;
+    if (success) {
+      resolve(car);
+    } else {
+      reject("rejected due to error");
+    }
+  }, 1000);
+});
 
 promise
-.then(data=>console.log("these are your car details",data))
-.catch(error=>console.log(error))
+  .then((data) => console.log("these are your car details", data))
+  .catch((error) => console.log(error));
