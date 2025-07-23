be130bef6ab81748105d6d9123b391d860e1f8a6
Shivam8299 <shivammishra0381@gmail.com>
2025-07-23 10:33:40 +0530

    Merge two sorted array into one array

--- mergeTwoSortedArr.js
+++ mergeTwoSortedArr.js
@@ -1,1 +1,27 @@
+arr1 = [1, 3, 5, 7];
+arr2 = [2, 4, 6, 8];
+let result = [];
+let i =0;
+let j=0;
 
+while(i<arr1.length && j < arr2.length){
+    if(arr1[i] < arr2[j]){
+        result.push(arr1[i])
+        i++
+    }
+    else{
+        result.push(arr2[j])
+        j++
+    }
+}
+while(i< arr1.length){
+        result.push(arr1[i])
+        i++
+    }
+
+    while(j< arr2.length){
+        result.push(arr2[j])
+        j++
+    }
+
+console.log(result)
